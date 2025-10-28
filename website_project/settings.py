4ea183438741dc04fc6eccdd7315f1778cc05905
JemAndrew <149887251+JemAndrew@users.noreply.github.com>
2025-10-28 15:23:41 +0000

    got rid of render

--- website_project/settings.py
+++ website_project/settings.py
@@ -258,13 +258,3 @@ if DEBUG:
     if not ALLOWED_HOSTS or ALLOWED_HOSTS == ['']:
         ALLOWED_HOSTS = ['*']
 
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME, 'localhost', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = config(
-        'ALLOWED_HOSTS',
-        default='localhost,127.0.0.1',
-        cast=lambda v: [s.strip() for s in v.split(',')]
-    )
